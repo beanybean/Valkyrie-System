931e9e0d5e81dcee351630a226756c3882977990
beanybean <guerrachristopher91@gmail.com>
2018-02-08 14:16:43 -0800

    Damage Module
    Damage Module Variables added

--- Assets/DamageModule.cs
+++ Assets/DamageModule.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NewBehaviourScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public class DamageModule
+    {
+        enum Type { Wind, Water, Lightning, Earth, Fire, Light, Dark };
+
+        public float PhAtk;
+        public float MaAtk;
+        public float PhDef;
+        public float MaDef;
+        public float Res;
+        public float Spd;
+        public float Resistance;
+
+        /*void LowerAttribute(Type attribute, float amount)
+        {
+
+        }*/
+
+    }
+
+   
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
