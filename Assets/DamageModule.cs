32df68cab232d8c53e782de742d4310bb8e377e7
beanybean <guerrachristopher91@gmail.com>
2018-02-08 15:58:59 -0800

    damage module changes
    get, set lower, and raise attributes

--- Assets/DamageModule.cs
+++ Assets/DamageModule.cs
@@ -3,36 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class NewBehaviourScript : MonoBehaviour {
+public class DamageModule : MonoBehaviour {
 
-    public class DamageModule
+    public float PhAtk;
+    public float MaAtk;
+    public float PhDef;
+    public float MaDef;
+    public float Res;
+    public float Spd;
+
+    void lowerAttribute(string attribute, float amount)
     {
-        enum Type { Wind, Water, Lightning, Earth, Fire, Light, Dark };
-
-        public float PhAtk;
-        public float MaAtk;
-        public float PhDef;
-        public float MaDef;
-        public float Res;
-        public float Spd;
-        public float Resistance;
-
-        /*void LowerAttribute(Type attribute, float amount)
-        {
-
-        }*/
-
+        if (attribute == "PhAtk")
+            PhAtk -= amount;
+        else if (attribute == "MaAtk")
+            MaAtk -= amount;
+        else if (attribute == "PhDef")
+            PhDef -= amount;
+        else if (attribute == "MaDef")
+            MaDef -= amount;
+        else if (attribute == "Res")
+            Res -= amount;
+        else
+            Spd -= amount;
     }
 
-   
+    void raiseAttribute(string attribute, float amount)
+    {
+        if (attribute == "PhAtk")
+            PhAtk += amount;
+        else if (attribute == "MaAtk")
+            MaAtk += amount;
+        else if (attribute == "PhDef")
+            PhDef += amount;
+        else if (attribute == "MaDef")
+            MaDef += amount;
+        else if (attribute == "Res")
+            Res += amount;
+        else
+            Spd += amount;
+    }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    float getAttribute(string attribute)
+    {
+        if (attribute == "PhAtk")
+            return PhAtk;
+        else if (attribute == "MaAtk")
+            return MaAtk;
+        else if (attribute == "PhDef")
+            return PhDef;
+        else if (attribute == "MaDef")
+            return MaDef;
+        else if (attribute == "Res")
+            return Res;
+        else
+            return Spd;
+    }
+
+    void setAttribute(string attribute, float amount)
+    {
+        if (attribute == "PhAtk")
+            PhAtk = amount;
+        else if (attribute == "MaAtk")
+            MaAtk = amount;
+        else if (attribute == "PhDef")
+            PhDef = amount;
+        else if (attribute == "MaDef")
+            MaDef = amount;
+        else if (attribute == "Res")
+            Res = amount;
+        else
+            Spd = amount;
+    }
 }
